7065c35d0e8b732bed9802fcba4b57f27e34d2e1
Trisha Dubey <trishadubey39@gmail.com>
2023-07-27 09:24:51 +0530

    stack

--- Solutions/Parentheses_Sorted.java
+++ Solutions/Parentheses_Sorted.java
@@ -1,5 +1,7 @@
 package Solutions;
 
+import java.util.Stack;
+
 public class Parentheses_Sorted {
     public static void main(String[] args) {
         System.out.println("Validity : " + isValid("()[]{}"));
@@ -7,14 +9,21 @@ public class Parentheses_Sorted {
     }
 
     public static boolean isValid(String s) {
-        if (String.valueOf(s.charAt(s.length())) == ")" && (String.valueOf(s.charAt(s.length() - 1)) == "(")) {
-            return true;
-        } else if (String.valueOf(s.charAt(s.length())) == "}" && (String.valueOf(s.charAt(s.length() - 1)) == "{")) {
-            return true;
-        } else if (String.valueOf(s.charAt(s.length())) == "]" && (String.valueOf(s.charAt(s.length() - 1)) == "[")) {
-            return true;
-        } else
+        Stack<Character> stack = new Stack<Character>();
+        if (s.length() == 1) {
             return false;
+        }
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
 
     }
 }
