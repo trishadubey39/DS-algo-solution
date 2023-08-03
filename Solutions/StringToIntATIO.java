90d5c44018a7e95f4930b0e824d1cfaf52216e4e
trisha <trishadubey39@gmail.com>
2023-08-03 21:07:35 +0530

    stringtoInt

--- Solutions/StringToIntATIO.java
+++ Solutions/StringToIntATIO.java
@@ -4,28 +4,58 @@ public class StringToIntATIO {
     }
 
     public static int myAtoi(String s) {
+        s = s.trim();
+        boolean isNegative = false;
         int num = 0;
-        Boolean containsMinus = false;
-       for(int i =0;i<s.length();i++){
-        if((s.charAt(i)=='+' && s.charAt(i+1) =='-') ||( s.charAt(i)=='-'&& s.charAt(i+1) =='+' ) )
-            return 0;
-        else if(s.charAt(i)==' '||s.charAt(i)=='+'){
-            
-        }else if(s.charAt(i)=='.'){
-            break;
-        }
-       else if(s.charAt(i) =='-')
-            containsMinus = true;
-        else if(s.charAt(i) >= '0' && s.charAt(i)<='9' ){
-            num = num *10 + (s.charAt(i)-'0');
-            if(num > (Integer.MAX_VALUE - (s.charAt(i)-'0')) / 10)
-                return containsMinus?Integer.MIN_VALUE: Integer.MAX_VALUE;
+        int i = 0;
 
-        }else if(num ==0){
+        if (s.length() == 0) {
             return 0;
         }
+
+        if (s.charAt(0) == '-') {
+            isNegative = true;
+            i++;
+        } else if (s.charAt(0) == '+') {
+            i++;
+        }
+
+        for (; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) {
+                break;
+            }
+            int digit = Character.getNumericValue(c);
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            num = num * 10 + digit;
+        }
+
+        return isNegative ? -num : num;
+        
+    //     int num = 0;
+    //     Boolean containsMinus = false;
+    //    for(int i =0;i<s.length();i++){
+    //     if((s.charAt(i)=='+' && s.charAt(i+1) =='-') ||( s.charAt(i)=='-'&& s.charAt(i+1) =='+' ) )
+    //         return 0;
+    //     else if(s.charAt(i)==' '||s.charAt(i)=='+'){
             
-       }
-        return containsMinus?(int)num*-1 :(int)num;
+    //     }else if(s.charAt(i)=='.'){
+    //         break;
+    //     }
+    //    else if(s.charAt(i) =='-')
+    //         containsMinus = true;
+    //     else if(s.charAt(i) >= '0' && s.charAt(i)<='9' ){
+    //         num = num *10 + (s.charAt(i)-'0');
+    //         if(num > (Integer.MAX_VALUE - (s.charAt(i)-'0')) / 10)
+    //             return containsMinus?Integer.MIN_VALUE: Integer.MAX_VALUE;
+
+    //     }else if(num ==0){
+    //         return 0;
+    //     }
+            
+    //    }
+    //     return containsMinus?(int)num*-1 :(int)num;
     }
 }
