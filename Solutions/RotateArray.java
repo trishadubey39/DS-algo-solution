a2b09f5ff87b9072001deeeb5ac81dd5ed3832cb
trishadubey39 <trishadubey39@gmail.com>
2023-10-19 15:33:12 +0530

    string-backtracking

--- Solutions/RotateArray.java
+++ Solutions/RotateArray.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 public class RotateArray {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] arr = { 2,7,11,15};
         rotate(arr, 10);
+        twoSum(arr,9);
+        
     }
 
     public static void reverse(int nums[], int i, int j) {
@@ -26,5 +28,22 @@ public class RotateArray {
         reverse(nums, 0, nums.length - k - 1);
         reverse(nums, nums.length - k, nums.length - 1);
         reverse(nums, 0, nums.length - 1);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] counts = new int[2];
+        int l=0;
+        int j=nums.length-1;
+        while(l<j){
+            if(nums[l]+nums[j] ==target){
+                counts[0]=l;
+                counts[1]=j;
+                return counts;
+            }else{
+                l++;
+                j--;
+            }
+        }
+       return counts; 
     }
 }
