effc8ac46cbba4ad4017ed8e968a0d1255c1df61
trishadubey39 <trishadubey39@gmail.com>
2023-11-26 14:53:21 +0530

    sports-aalytics

--- Solutions/ReverseAString.java
+++ Solutions/ReverseAString.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class ReverseAString{
     public static void main(String[] args){
         char[] ca = {'b','a','c','d'};
-        reverseString(ca);
+        // reverseString(ca);
+
+    // System.out.println(isAnagram("anagram","nagaram"));
+    System.out.println(isPalindrome("A man, a plan, a canal -- Panama"));
     }
 
     public static void reverseString(char[] s) {
@@ -20,5 +25,30 @@ public class ReverseAString{
         }
 
     }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+
+    public static boolean isPalindrome(String s) {
+        String t = s.replaceAll("[^a-zA-Z0-9_-]","").replaceAll("[_+$=-]", "").toLowerCase();
+        int m =0 , n = t.length()-1;
+
+        while(m<n){
+            if(t.charAt(m)!=(t.charAt(n))){
+                return false;
+            }
+            m++;
+            n--;
+        }
+        return true;
+    }
+    
 
 }
