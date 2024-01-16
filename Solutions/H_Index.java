fd7a2e999f1a30ad8d4069029ef7d2562b9a53cb
trishadubey39 <trishadubey39@gmail.com>
2024-01-16 18:44:19 +0530

    h-idex

---
+++ Solutions/H_Index.java
@@ -0,0 +1,26 @@
+public class H_Index {
+    public static void main(String[] args) {
+        int[] nums = { 3, 0, 6, 1, 5 };
+        System.out.println(hIndex(nums));
+    }
+
+    public static int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+        for (int c : citations) {
+            if (c >= n) {
+                buckets[n]++;
+            } else {
+                buckets[c]++;
+            }
+        }
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
