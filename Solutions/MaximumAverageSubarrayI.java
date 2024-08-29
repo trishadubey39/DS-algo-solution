ece7881eeb5e3ba809b124973b814ef1b384552b
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2024-08-29 14:55:05 +0530

    Update MaximumAverageSubarrayI.java
    maximum sub array

--- Solutions/MaximumAverageSubarrayI.java
+++ Solutions/MaximumAverageSubarrayI.java
@@ -1,12 +1,12 @@
 public class MaximumAverageSubarrayI {
     public static void main(String[] args) {
 
-        int[] nums2 = {1,12,-5,-6,50,3};
+        int[] nums2 = {0,1,1,3,3};
         System.out.println(maximumAverageSubarrayI(nums2,4));
     }
 
     public static float maximumAverageSubarrayI(int[]nums, int k){
-        float max_avg = 0;
+        float max_avg = Integer.MIN_VALUE;
         float curr_avg = 0;
         float last_sum = 0;
 
@@ -17,7 +17,7 @@ public class MaximumAverageSubarrayI {
         curr_avg = (last_sum )/ k;
         max_avg = Math.max(curr_avg, max_avg);
 
-        for (int i = 1; i < nums.length - k; i++) {
+        for (int i = 1; i <= nums.length - k; i++) {
             last_sum =(last_sum- nums[i-1]) + nums[i+k-1];
             curr_avg = last_sum  / k;
             max_avg = Math.max(curr_avg, max_avg);
